2c84f451205faccf944b33155a6ed19351c580d4
IgorKulchitskiy <igorkulch07@gmail.com>
2024-03-11 12:44:16 +0200

    Binarytreelab
    Binarytreelab

---
+++ algorythm_and_programing_part_2/lab_binarytree/tree.py
@@ -0,0 +1,21 @@
+class TreeNode:
+    def __init__(self, value=0, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def sum_of_depths(root: TreeNode) -> int:
+    def depth_sum(node, depth):
+        if node is None:
+            return 0
+        return depth + depth_sum(node.left, depth + 1) + depth_sum(node.right, depth + 1)
+    
+    return depth_sum(root, 0)
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+print(sum_of_depths(root))
