9e0fb5c7d3b4cf64a18fc4b05c73e07b0b8abfca
IgorKulchitskiy <igorkulch07@gmail.com>
2024-03-12 14:22:32 +0200

    test_find_pair_that_equal_to_target.py
    test_find_pair_that_equal_to_target.py

---
+++ test/test_find_pair_that_equal_to_target.py
@@ -0,0 +1,20 @@
+import unittest
+from def_find_pair_with_sum import find_pair_which_equal_target
+
+class Test(unittest.TestCase):
+    def test_true(self):
+        numbers = [ 2 , 7 , 11 , 15]
+        target = 9 
+        result = find_pair_which_equal_target(numbers , target)
+        self.assertIsNotNone(result)
+        self.assertEqual(result, (0, 1))
+
+
+    def test_false(self):
+        numbers = [3 , 5]
+        target = 6
+        result = find_pair_which_equal_target(numbers , target)
+        self.assertIsNone(result)
+
+if __name__ == '__main__':
+    unittest.main()
