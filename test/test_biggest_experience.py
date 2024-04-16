c8a0d73039126437d9d54613560cfd168836deda
IgorKulchitskiy <igorkulch07@gmail.com>
2024-04-16 14:06:13 +0300

    chaged algorithm after Veres told

--- test/test_biggest_experience.py
+++ test/test_biggest_experience.py
@@ -1,18 +1,31 @@
 import unittest
-from src.find_the_biggest_experience import max_experience
+from src.find_the_biggest_experience import maximum_experience
 
 class TestMaxExperience(unittest.TestCase):
-
-    def test_max_experience_first_level(self):
-        levels = 1
-        experience = [[9999]]
-        self.assertEqual(max_experience(levels, experience), 9999)
-
-    def test_max_experience_second_level(self):
+    def test_small_example(self):
         levels = 2
-        experience = [[1], [8, 6]]
-        self.assertEqual(max_experience(levels, experience), 13)
+        experience = [
+            [2],
+            [2, 3]
+        ]
+        self.assertEqual(maximum_experience(levels, experience), 5)
 
+    def test_max_path(self):
+        levels = 4
+        experience = [
+            [7],
+            [7, 8],
+            [2, 1, 9],
+            [3, 0, 0, 5]
+        ]
+        self.assertEqual(maximum_experience(levels, experience), 29)
+
+    def test_high_value(self):
+        levels = 1
+        experience = [
+            [9999]
+        ]
+        self.assertEqual(maximum_experience(levels, experience), 9999)
 
 if __name__ == '__main__':
     unittest.main()
