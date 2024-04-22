459b11225e67b5331d9bf73a9b4620036606ee49
IgorKulchitskiy <igorkulch07@gmail.com>
2024-04-22 14:00:03 +0300

    new lab 7
    code of lab7

---
+++ src/searck_of_morris_prat.py
@@ -0,0 +1,47 @@
+def compute_lps_array(pattern, m):
+    lps = [0] * m
+    length = 0
+    i = 1
+    while i < m:
+        if pattern[i] == pattern[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        else:
+            if length != 0:
+                length = lps[length - 1]
+            else:
+                lps[i] = 0
+                i += 1
+    return lps
+
+def kmp_search(text, pattern):
+    m = len(pattern)
+    n = len(text)
+    indices = []
+
+    lps = compute_lps_array(pattern, m)
+
+    i = 0
+    j = 0
+    while i < n:
+        if pattern[j] == text[i]:
+            i += 1
+            j += 1
+
+        if j == m:
+            indices.append(i - j)
+            j = lps[j - 1]
+
+        elif i < n and pattern[j] != text[i]:
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+
+    return indices
+
+# Приклад використання функції kmp_search
+text = "Привіт Василь"
+pattern = "Василь"
+print("Індекси входжень:", kmp_search(text, pattern))
---
+++ test/test_search_of_morris_prat.py
@@ -0,0 +1,26 @@
+import unittest
+from src.searck_of_morris_prat import kmp_search
+
+class TestKMPSearch(unittest.TestCase):
+    def test_single_match(self):
+        text = "Hello, world!"
+        pattern = "world"
+        self.assertEqual(kmp_search(text, pattern), [7])
+
+    def test_multiple_matches(self):
+        text = "ababcababcab"
+        pattern = "ab"
+        self.assertEqual(kmp_search(text, pattern), [0, 2, 5, 7, 10])
+
+    def test_no_match(self):
+        text = "Python is awesome"
+        pattern = "Java"
+        self.assertEqual(kmp_search(text, pattern), [])
+
+    def test_empty_text(self):
+        text = ""
+        pattern = "pattern"
+        self.assertEqual(kmp_search(text, pattern), [])
+
+if __name__ == "__main__":
+    unittest.main()
