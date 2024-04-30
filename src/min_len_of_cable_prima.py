f795cb09879ca1508bfa16676d55485e2076c2b7
IgorKulchitskiy <igorkulch07@gmail.com>
2024-04-30 17:07:10 +0300

    added priority queue

--- src/min_len_of_cable_prima.py
+++ src/min_len_of_cable_prima.py
@@ -1,5 +1,7 @@
-#8
-def read_input(file_path):
+from typing import Dict, List, Tuple
+from queue import PriorityQueue
+
+def read_input(file_path: str) -> Dict[str, List[Tuple[str, int]]]:
     graph = {}
     with open(file_path, 'r') as file:
         for line in file:
@@ -13,8 +15,7 @@ def read_input(file_path):
             graph[well2].append((well1, distance))
     return graph
 
-
-def prim(graph):
+def prim(graph: Dict[str, List[Tuple[str, int]]]) -> int:
     visited = set()
     min_prim_tree = []
     start_node = next(iter(graph))
@@ -22,21 +23,24 @@ def prim(graph):
     edges = [(cost, start_node, neighbor) for neighbor, cost in graph[start_node]]
     edges.sort()
 
-    while edges:
-        cost, current, next_node = edges.pop(0)
+    priority_queue = PriorityQueue()
+
+    for edge in edges:
+        priority_queue.put(edge)
+
+    while not priority_queue.empty():
+        cost, current, next_node = priority_queue.get()
         if next_node not in visited:
             visited.add(next_node)
             min_prim_tree.append((current, next_node, cost))
             for neighbor, n_cost in graph[next_node]:
                 if neighbor not in visited:
-                    edges.append((n_cost, next_node, neighbor))
-            edges.sort()
+                    priority_queue.put((n_cost, next_node, neighbor))
 
     if len(visited) != len(graph):
         return -1
     else:
         return sum(cost for _, _, cost in min_prim_tree)
-
 
 file_path = "communication_wells.csv"
 graph = read_input(file_path)
